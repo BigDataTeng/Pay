7a47fd17a960039ba1beaf841776e3937ea6357f
BigDataTeng <1131351369@qq.com>
2019-08-28 16:41:52 +0800

    Initial commit

---
+++ src/main/scala/com/Utils/JedisConnectionPool.scala
@@ -0,0 +1,20 @@
+package com.Utils
+
+import redis.clients.jedis.{Jedis, JedisPool, JedisPoolConfig}
+
+/**
+  * redis连接池
+  */
+object JedisConnectionPool {
+  val config = new JedisPoolConfig
+
+  config.setMaxTotal(30)// 最大连接
+
+  config.setMaxIdle(10)// 最大空闲
+
+  val pool = new JedisPool(config,"hadoop01",6379,10000)
+  // 获取Jedis对象
+  def getConnection():Jedis={
+    pool.getResource
+  }
+}
